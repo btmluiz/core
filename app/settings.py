4b1c7ade54efa752f0e8db9799e274481e510010
Hecjin <atiliogoulart@gmail.com>
2021-11-16 18:06:31 -0300

    Update the apps structure

--- app/settings.py
+++ app/settings.py
@@ -27,8 +27,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
-    'app.cliente',
-    'app.vendedor',
+    'app.client',
+    'app.seller',
 ]
 
 MIDDLEWARE = [
--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/client/', include('app.client.urls')),
+    path('api/seller/', include('app.seller.urls')),
 ]
