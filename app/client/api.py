dcedafeba42935023b8540738c694bb7c9991788
Hector <atiliogoulart@gmail.com>
2021-11-30 14:23:38 -0300

    Add auth

--- app/client/api.py
+++ app/client/api.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework import permissions
 
 from .models import ClientModel
 from .serializer import ClientSerializer
@@ -7,3 +8,4 @@ from .serializer import ClientSerializer
 class ClientViewSet(viewsets.ModelViewSet):
     serializer_class = ClientSerializer
     queryset = ClientModel.objects.all()
+    permission_classes = (permissions.IsAuthenticated,)
