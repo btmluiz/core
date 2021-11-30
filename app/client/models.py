02e4b11d00af34deda4496f3b41c909241199b63
Hector <atiliogoulart@gmail.com>
2021-11-30 14:11:37 -0300

    Update client models

--- app/client/models.py
+++ app/client/models.py
@@ -1,10 +1,16 @@
 from django.db import models
+import uuid
+from django.urls import reverse
 
 
 class ClientModel(models.Model):
 
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField("Name", max_length=55)
     email = models.EmailField("Email", max_length=100)
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse("client_detail", args=[str(self.id)])
