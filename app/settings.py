048e44859a2aca7086d63c56b7abc45642773d8b
Hector <atiliogoulart@gmail.com>
2021-11-30 14:17:49 -0300

    Add pagination

--- app/settings.py
+++ app/settings.py
@@ -57,6 +57,11 @@ TEMPLATES = [
     }
 ]
 
+REST_FRAMEWORK = {
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 100,
+}
+
 WSGI_APPLICATION = "app.wsgi.application"
 
 
